bb4d8881e8762fa430d4b22530a095de0f12b766
Maximiliano Carrizo <maximiliano.carrizo@whiteprompt.com>
2021-03-22 18:28:06 -0300

    Swagger

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # nati-papelera-backend
 Backend Nati Papelera
+
+1) Coverage ...%
+2) Full typed project. No any type. Enforced by tslint rules
+3) Tests with decorators instead of describe->it
+4) Swagger documentation already configured
---
+++ src/helpers/swagger.ts
@@ -0,0 +1,14 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export const setupSwagger = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('Nati Papelera Backend')
+    // .setDescription('The pepep API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+};
