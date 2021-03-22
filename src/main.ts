1cdab652200bfe28de3c8951fb2fe9b38302a1dd
Maximiliano Ezequiel Carrizo <mcarrizo@gmail.com>
2021-03-22 18:29:01 -0300

    Swagger
    Co-authored-by: Maximiliano Carrizo <maximiliano.carrizo@whiteprompt.com>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './helpers/swagger';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app)
   await app.listen(3000);
 };
 bootstrap();
