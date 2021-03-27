b294cb250ad578b37c4eb3e1ef1e423aa0b71278
Maximiliano Carrizo <maximiliano.carrizo@whiteprompt.com>
2021-03-27 16:53:09 -0300

    .

--- test/src/tests/auth.test.ts
+++ test/src/tests/auth.test.ts
@@ -2,6 +2,7 @@ import { TestSuite, Test } from '../../helpers/decorators';
 import { AbstractTestSuite } from '../abstract-test-suite';
 import { HttpStatus } from '@nestjs/common';
 import * as expect from 'expect';
+import * as faker from 'faker';
 @TestSuite('Auth Suite')
 export class AuthTest extends AbstractTestSuite {
   @Test('Login Successful')
@@ -24,5 +25,43 @@ export class AuthTest extends AbstractTestSuite {
   public async invalidUsernameOrPassword() {
     const dto = { emailAddress: 'test@test.com', password: 'test1' };
     await this.httpPost('/auth/login').send(dto).expect(HttpStatus.UNAUTHORIZED);
+  }
+  @Test('Create with Admin User')
+  public async createAdmin() {
+    const dto = {
+      name: faker.random.words(),
+      url: faker.internet.url(),
+    };
+    await this.httpAdminPost('/categories/create').send(dto).expect(HttpStatus.CREATED);
+  }
+
+  @Test('Create with User User')
+  public async createUser() {
+    const dto = {
+      name: faker.random.words(),
+      url: faker.internet.url(),
+    };
+    await this.httpUserPost('/categories/create').send(dto).expect(HttpStatus.FORBIDDEN);
+  }
+
+  @Test('Create with Not Logged in User')
+  public async createNotLoggedIn() {
+    const dto = {
+      name: faker.random.words(),
+      url: faker.internet.url(),
+    };
+    await this.httpPost('/categories/create').send(dto).expect(HttpStatus.UNAUTHORIZED);
+  }
+
+  @Test('Create with Invalid JWT Token')
+  public async createInvalidToken() {
+    const dto = {
+      name: faker.random.words(),
+      url: faker.internet.url(),
+    };
+    return this.httpPost('/categories/create')
+      .set('Authorization', `Bearer INVALID`)
+      .send(dto)
+      .expect(HttpStatus.UNAUTHORIZED);
   }
 }
--- test/src/tests/categories.test.ts
+++ test/src/tests/categories.test.ts
@@ -1,5 +1,4 @@
 import * as expect from 'expect';
-import * as faker from 'faker';
 import { TestSuite, Test } from '../../helpers/decorators';
 import { AbstractTestSuite } from '../abstract-test-suite';
 import { HttpStatus } from '@nestjs/common';
@@ -9,44 +8,5 @@ export class CategoriesTest extends AbstractTestSuite {
   public async getAll() {
     const { body: categories } = await this.httpGet('/categories/get-all').expect(HttpStatus.OK);
     expect(categories.length).toBeGreaterThan(0);
-  }
-
-  @Test('Create with Admin User')
-  public async createAdmin() {
-    const dto = {
-      name: faker.random.words(),
-      url: faker.internet.url(),
-    };
-    await this.httpAdminPost('/categories/create').send(dto).expect(HttpStatus.CREATED);
-  }
-
-  @Test('Create with User User')
-  public async createUser() {
-    const dto = {
-      name: faker.random.words(),
-      url: faker.internet.url(),
-    };
-    await this.httpUserPost('/categories/create').send(dto).expect(HttpStatus.FORBIDDEN);
-  }
-
-  @Test('Create with Not Logged in User')
-  public async createNotLoggedIn() {
-    const dto = {
-      name: faker.random.words(),
-      url: faker.internet.url(),
-    };
-    await this.httpPost('/categories/create').send(dto).expect(HttpStatus.UNAUTHORIZED);
-  }
-
-  @Test('Create with Invalid JWT Token')
-  public async createInvalidToken() {
-    const dto = {
-      name: faker.random.words(),
-      url: faker.internet.url(),
-    };
-    return this.httpPost('/categories/create')
-      .set('Authorization', `Bearer INVALID`)
-      .send(dto)
-      .expect(HttpStatus.UNAUTHORIZED);
   }
 }
