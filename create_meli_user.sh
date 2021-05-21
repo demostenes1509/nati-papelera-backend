efc590c6dc9ab377fb6306d892241883a0ca2851
Maximiliano Ezequiel Carrizo <mcarrizo@gmail.com>
2021-05-21 13:22:40 -0300

    Delete create_meli_user.sh

--- create_meli_user.sh
+++
@@ -1,3 +0,0 @@
-ACCESS_TOKEN=APP_USR-8567416418689323-052113-879938a31233b00c2f0ebd825c8951b8-62636656
-curl -X POST -H 'Authorization: Bearer $ACCESS_TOKEN' -H "Content-type: application/json" -d '{ "site_id":"MLA" }' 'https://api.mercadolibre.com/users/test_user'
-
