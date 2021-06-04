68b48b0e6360f5da2f4c6e8a68066bd4c3ff1955
fbauza <hugo_1962@live.com.ar>
2021-06-04 04:25:39 -0300

    update Provider percentage test

--- test/src/tests/providers.test.ts
+++ test/src/tests/providers.test.ts
@@ -130,6 +130,6 @@ export class ProvidersTest extends AbstractTestSuite {
   public async ProviderRangePercentage() {
     const dto = { id: uuidv4(), name: faker.company.companyName(), url: faker.internet.url(), percentage: 300 };
 
-    await this.httpAdminPut('/providers/provider-update/').send(dto).expect(HttpStatus.BAD_REQUEST);
+    const m = await this.httpAdminPut('/providers/provider-update/').send(dto).expect(HttpStatus.BAD_REQUEST);
   }
 }
