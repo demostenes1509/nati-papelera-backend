ec0c73a967e3b8da7372d27b8e5d6bf35be1a3f5
fbauza <hugo_1962@live.com.ar>
2021-06-26 19:42:48 -0300

    update comments

--- test/src/tests/configuration.test.ts
+++ test/src/tests/configuration.test.ts
@@ -1,31 +1,28 @@
 import { Body, HttpStatus } from '@nestjs/common';
 import * as expect from 'expect';
-import { Configuration } from 'src/models';
+//import { Configuration } from 'src/models';
 import { Test, TestSuite } from '../../helpers/decorators';
 import { AbstractTestSuite } from '../abstract-test-suite';
 import { v4 as uuidv4 } from 'uuid';
-import { GetConfigurationResponseDto } from '../../../src/modules/configuration/dto/configuration-get-response.dto';
-import { ConfigurationRequestDto } from 'src/modules/configuration/dto/configuration-update-request.dto';
+//import { GetConfigurationResponseDto } from '../../../src/modules/configuration/dto/configuration-get-response.dto';
+//import { ConfigurationRequestDto } from 'src/modules/configuration/dto/configuration-update-request.dto';
 
 @TestSuite('Configuration Test')
 export class ConfigurationTest extends AbstractTestSuite {
     @Test('Configuration get')
     public async testConfigurationGet() {
-        const { body: configDto } = await this.httpAdminGet('/configuration/get').expect(HttpStatus.OK);
-        expect(configDto.configuration.id.length).toBeGreaterThan(0);
+        const { body: configuration } = await this.httpAdminGet('/configuration/get').expect(HttpStatus.OK);
+        expect(configuration.configuration.id.length).toBeGreaterThan(0);
     }
 
     @Test('Configuration update')
     public async testConfigurationUpdate() {
-        const { body: dtoGet } = await this.httpAdminGet('/configuration/get')
-        .expect(HttpStatus.OK);
+        const { body: dtoGet } = await this.httpAdminGet('/configuration/get').expect(HttpStatus.OK);
         expect(dtoGet.configuration.id.length).toBeGreaterThan(0);
 
         const dtoToUpdate = { id: dtoGet.configuration.id,
                               mlCommissionPercentage: Math.random() * (200 - 1) + 1,
-                              mlGainPercentage: Math.random() * (200 - 1) + 1
-        };
-
+                              mlGainPercentage: Math.random() * (200 - 1) + 1 };
         await this.httpAdminPut('/configuration/update').send(dtoToUpdate).expect(HttpStatus.OK);
     }
 
@@ -41,8 +38,7 @@ export class ConfigurationTest extends AbstractTestSuite {
     @Test('Configuration invalid commission percentage')
     public async testInvalidCommissionPercentage(){
         const dto = {id: uuidv4(), mlCommissionPercentage: 300, mlGainPercentage: 100};
-        const {body: messages} = await this.httpAdminPut('/configuration/update')
-            .send(dto).expect(HttpStatus.BAD_REQUEST);
+        const {body: messages} = await this.httpAdminPut('/configuration/update').send(dto).expect(HttpStatus.BAD_REQUEST);
         expect(messages.length).toBe(1);
         expect(messages[0]).toBe('mlCommissionPercentage must not be greater than 200');
     }
@@ -50,8 +46,7 @@ export class ConfigurationTest extends AbstractTestSuite {
     @Test('Configuration invalid gain percentage')
     public async testInvalidGainPercentage(){
         const dto = {id: uuidv4(), mlCommissionPercentage: 100, mlGainPercentage: 300};
-        const {body: messages} = await this.httpAdminPut('/configuration/update')
-            .send(dto).expect(HttpStatus.BAD_REQUEST);
+        const {body: messages} = await this.httpAdminPut('/configuration/update').send(dto).expect(HttpStatus.BAD_REQUEST);
         expect(messages.length).toBe(1);
         expect(messages[0]).toBe('mlGainPercentage must not be greater than 200');
     }
