ce111dbd2e684021ce531d6dda16e1141e180b3b
fbauza <hugo_1962@live.com.ar>
2021-07-15 20:24:16 -0300

    fix

--- src/modules/configuration/dto/configuration-update-request.dto.ts
+++ src/modules/configuration/dto/configuration-update-request.dto.ts
@@ -1,6 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsUUID, Min, Max } from 'class-validator';
-import { Configuration } from '../../../models/configuration.entity';
 
 export class ConfigurationRequestDto {
   @ApiProperty()
@@ -13,17 +12,9 @@ export class ConfigurationRequestDto {
   @Max(200)
   mlCommissionPercentage: number;
 
-<<<<<<< HEAD
   @ApiProperty()
   @IsNumber()
   @Min(0)
   @Max(200)
   mlGainPercentage: number;
-=======
-    @ApiProperty()
-    @IsNumber()
-    @Min(0)
-    @Max(200)
-    mlGainPercentage: number;
->>>>>>> 188b3cba93a41736458b9bff9d2c326094b89565
 }
